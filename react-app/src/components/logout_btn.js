a871a285addc222a10ee4d388ebf10a31155bd1d
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-09-06 12:22:24 +0700

    Log out btn

---
+++ react-app/src/components/logout_btn.js
@@ -0,0 +1,17 @@
+import { NavbarBtn } from "./navbar_btn";
+import { setCookie } from "../api/cookie"
+
+function LogoutBtn() 
+{
+    const onClick = () =>
+    {
+        if ( window.confirm('All current on page data won\'t be saved') )
+        {
+            setCookie('auth',"",0)
+        }
+    }
+
+    return <NavbarBtn onClick={onClick} /> 
+}
+
+export { LogoutBtn } ;
