e6d9f3946f6d8a2ae713bd4173fd1fdff3306da1
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-09-08 12:51:10 +0700

    format

--- react-app/src/pages/contest_page.js
+++ react-app/src/pages/contest_page.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Navigationbar } from "../components/navbar";
 import { useEffect } from "react";
+import { UserEditor } from "../components/ace_editor";
 
 function ContestPage ()
 {
@@ -10,7 +11,10 @@ function ContestPage ()
         console.log(contestId)
       }, [contestId])
     
-    return (<Navigationbar />) 
+    return (<>
+        <Navigationbar />
+         
+    </>)
 
 }
 
