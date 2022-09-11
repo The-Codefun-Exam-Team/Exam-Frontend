e7d5f3ea757507271c316a3acff9ada5c785d0cd
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-09-11 16:25:33 +0700

    add api path

--- react-app/src/api/codefundebug.js
+++ react-app/src/api/codefundebug.js
@@ -5,7 +5,7 @@ function getDebugProblem (debugProblemId)
     var return_data = 'Fail' ;
     $.ajax({
         type: 'GET',
-        url: 'http://bugsad.ddns.net/debug_problem',
+        url: 'http://bugsad.ddns.net/api/debug_problem',
         async: false ,
         success(data,status){
             console.log('Get problem infor status: ' + status ) ;
@@ -22,7 +22,7 @@ function getSubmission (submissionId)
     var return_data = 'Fail' ;
     $.ajax({
         type: 'GET',
-        url: 'http://bugsad.ddns.net/debug_submission/' + submissionId,
+        url: 'http://bugsad.ddns.net/api/debug_submission/' + submissionId,
         async: false,
         success(data,status){
             console.log('Get submission infor status: ' + status ) ;
