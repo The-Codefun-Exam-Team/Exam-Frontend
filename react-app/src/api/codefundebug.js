c5a0357843d7bb7e100d0431d8860093d11aec65
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-09-12 10:08:02 +0700

    change domain name

--- react-app/src/api/codefundebug.js
+++ react-app/src/api/codefundebug.js
@@ -5,7 +5,7 @@ function getDebugProblem (debugProblemId)
     var return_data = 'Fail' ;
     $.ajax({
         type: 'GET',
-        url: 'http://bugsad.ddns.net/api/problems/' + debugProblemId,
+        url: 'https://112.137.129.136:21700/api/problems/' + debugProblemId,
         async: false ,
         success(data,status){
             console.log('Get problem infor status: ' + status ) ;
@@ -22,7 +22,7 @@ function getSubmission (submissionId)
     var return_data = 'Fail' ;
     $.ajax({
         type: 'GET',
-        url: 'http://bugsad.ddns.net/api/debug_submission/' + submissionId,
+        url: 'https://112.137.129.136:21700/api/debug_submission/' + submissionId,
         async: false,
         success(data,status){
             console.log('Get submission infor status: ' + status ) ;
