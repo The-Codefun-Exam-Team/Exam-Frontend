fc07703f45b422974eaad1351c79721007a7a3be
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-10-06 15:51:22 +0700

    api method

--- react-app/src/components/ranktable.js
+++ react-app/src/components/ranktable.js
@@ -1,11 +1,54 @@
 import colors from '../config/color.ts'
+import axios from 'axios'
+import { useEffect , useState , useCallback} from 'react';
+import { getCookie } from '../api/cookie';
+
+const useAxios = ({ url, method, body = null, headers = null }) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setloading] = useState(true);
+    
+    const fetchData = useCallback(() => {
+        axios[method](url, JSON.parse(headers), JSON.parse(body))
+            .then((res) => {
+                setResponse(res.data);
+            })
+            .catch((err) => {
+                setError(err);
+            })
+            .finally(() => {
+                setloading(false);
+            });
+    },[body,method,url,headers]);
+
+    useEffect(() => {
+        fetchData();
+    }, [method, url, body, headers, fetchData]);
+
+    return { response, error, loading };
+};
 
 function RankTable ()
 {
-    
-    return <>
-        <div>lorem ipsum</div>
-    </>
+    const {response,error,loading} = useAxios(
+        {
+            url:`https://debug.codefun.vn/api/rankings?group=${0}&page=${1}&limit=${50}`,
+            method:'get',
+            headers: JSON.stringify({
+                Authorization: 'Bearer ' + getCookie('auth') 
+            }),
+            // body : JSON.stringify({
+            //     group: 0,
+            //     page: 1,
+            //     limit: 50,
+
+            // })
+        }
+    )
+    console.log(response)
+    return (
+        <div>lorem</div>
+    )
 }
 
 export {RankTable}
